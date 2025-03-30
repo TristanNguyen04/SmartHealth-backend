9535e7b3748ddeb486c5c83f571edbf655d0a681
TristanNguyen04 <nquocdung.04@gmail.com>
2025-03-30 20:14:34 +0800

    Test Authentication

--- src/main/java/com/SmartHealth/SmartHealth_backend/config/SecurityConfig.java
+++ src/main/java/com/SmartHealth/SmartHealth_backend/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.SmartHealth.SmartHealth_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,8 +20,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(basic -> {});  // Updated HTTP Basic configuration
-
+                .httpBasic(Customizer.withDefaults())  // Updated HTTP Basic configuration
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
